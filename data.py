1136324fffa5e2478f24793f5fa5a1afafc071ff
yoyomaster9 <yoyomaster9@yahoo.com>
2020-04-08 14:57:44 -0700

    Small adjustments

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 admins = [241703543017308162, 107299565551529984]
 
-clockInTime = ['Monday 14:20', 'Tuesday 14:20', 'Wednesday 14:20', 'Thursday 14:20', 'Saturday 9:20', 'Sunday 11:20']
+clockInTime = ['Monday 14:25', 'Tuesday 14:25', 'Wednesday 14:25', 'Thursday 14:25', 'Saturday 9:25', 'Sunday 11:25']
 
 clockOutTime = ['Monday 20:00', 'Tuesday 20:00', 'Wednesday 20:00', 'Thursday 20:00', 'Saturday 15:00', 'Sunday 15:00']
 
