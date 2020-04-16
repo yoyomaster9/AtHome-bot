c2d4bf17ee298d8a007cc792fa131946215c3105
yoyomaster9 <yoyomaster9@yahoo.com>
2020-04-15 18:09:23 -0700

    Fixed military time bug

--- data.py
+++ data.py
@@ -4,7 +4,7 @@ clockInTime = ['Monday 14:25', 'Tuesday 14:25', 'Wednesday 14:25', 'Thursday 14:
 
 clockOutTime = ['Monday 20:00', 'Tuesday 20:00', 'Wednesday 20:00', 'Thursday 20:00', 'Saturday 15:00', 'Sunday 15:00']
 
-workoutPlanTime = [d + ' ' + t for d in ['Monday', 'Tuesday', 'Wednesday', 'Thursday'] for t in ['3:55', '4:55', '5:55', '6:55']] + [ 'Saturday ' + t for t in ['10:55', '11:55', '12:55', '13:55']] + ['Sunday ' + t for t in ['12:55', '13:55']]
+workoutPlanTime = [d + ' ' + t for d in ['Monday', 'Tuesday', 'Wednesday', 'Thursday'] for t in ['15:55', '16:55', '17:55', '18:55']] + [ 'Saturday ' + t for t in ['10:55', '11:55', '12:55', '13:55']] + ['Sunday ' + t for t in ['12:55', '13:55']]
 
 notificationChannelID = 694716112859955220
 
