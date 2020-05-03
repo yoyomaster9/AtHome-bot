a888c69ec01012794bca255946527fd890b43cce
yoyomaster9 <yoyomaster9@yahoo.com>
2020-05-03 14:59:03 -0700

    Added wobTimes

--- data.py
+++ data.py
@@ -4,9 +4,9 @@ clockInTime = ['Monday 14:30', 'Tuesday 14:30', 'Wednesday 14:30', 'Thursday 14:
 
 clockOutTime = ['Monday 20:00', 'Tuesday 20:00', 'Wednesday 20:00', 'Thursday 20:00', 'Saturday 15:00', 'Sunday 15:00']
 
-workoutPlanTime = [d + ' ' + t for d in ['Monday', 'Tuesday', 'Wednesday', 'Thursday'] for t in ['15:55', '16:55', '17:55', '18:55']] + [ 'Saturday ' + t for t in ['10:55', '11:55', '12:55', '13:55']] + ['Sunday ' + t for t in ['12:55', '13:55']]
+workoutPlanTime = [d + ' ' + t for d in ['Monday', 'Tuesday', 'Wednesday', 'Thursday'] for t in ['15:55', '16:55', '17:55', '18:55']] + [ 'Saturday ' + t for t in ['10:55', '11:55', '12:55', '13:55', ]] + ['Sunday ' + t for t in ['12:55', '13:55']]
 
-wobTime = [d + ' ' + t for d in ['Monday', 'Tuesday', 'Wednesday', 'Thursday'] for t in ['15:35', '16:35', '17:35', '18:35']] + [ 'Saturday ' + t for t in ['10:35', '11:35', '12:35', '13:35']] + ['Sunday ' + t for t in ['12:35', '13:35']]
+wobTime = [d + ' ' + t for d in ['Monday', 'Tuesday', 'Wednesday', 'Thursday'] for t in ['15:35', '16:35', '17:35', '18:35', '19:35']] + [ 'Saturday ' + t for t in ['10:35', '11:35', '12:35', '13:35', '14:35']] + ['Sunday ' + t for t in ['12:35', '13:35', '14:35']]
 
 notificationChannelID = 694716112859955220
 
