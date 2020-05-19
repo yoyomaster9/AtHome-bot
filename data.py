85adb61daf38af6c67ae3f0b457285e008954bea
yoyomaster9 <yoyomaster9@yahoo.com>
2020-05-19 11:46:12 -0700

    Time adjustment

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 admins = [241703543017308162, 107299565551529984]
 
-clockInTime = ['Monday 14:30', 'Tuesday 14:30', 'Wednesday 14:30', 'Thursday 14:30', 'Saturday 09:30', 'Sunday 11:30']
+clockInTime = ['Monday 14:45', 'Tuesday 14:45', 'Wednesday 14:45', 'Thursday 14:45', 'Saturday 09:45', 'Sunday 11:30']
 
 clockOutTime = ['Monday 20:00', 'Tuesday 20:00', 'Wednesday 20:00', 'Thursday 20:00', 'Saturday 15:00', 'Sunday 15:00']
 
