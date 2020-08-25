b1c539899bc0d998d74967ae8abb5846034fe080
yoyomaster9 <yoyomaster9@yahoo.com>
2020-08-25 15:13:46 -0700

    Oops

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 admins = [241703543017308162, 107299565551529984]
 
-wobTime = [d + ' ' + t for d in ['Monday', 'Tuesday', 'Wednesday', 'Thursday'] for t in ['15:35', '16:35', '17:35', '18:35', '19:35']]
+workoutPlanTime = [d + ' ' + t for d in ['Monday', 'Tuesday', 'Wednesday', 'Thursday'] for t in ['15:55', '16:55', '17:55', '18:55']] + ['Saturday ' + t for t in ['10:55', '11:55', '12:55', '1:55', '2:55']] + ['Sunday ' + t for t in ['12:55', '1:55', '2:55']]
 
 notificationChannelID = 694716112859955220
 
